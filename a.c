696968eed06350ca2b01a7c90677773bb0eb579b
zhouyuan <3623936813@qq.com>
2020-10-10 16:05:26 +0800

    a.c

---
+++ a.c
@@ -0,0 +1,18 @@
+/*************************************************************************
+	> File Name: a.c
+	> Author: zhouyuan
+	> Mail: 3294207721@qq.com 
+	> Created Time: 2020年10月10日 星期六 16时04分27秒
+ ************************************************************************/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<math.h>
+#include<time.h>
+#include<unistd.h>
+ 
+int main() {
+    printf("Hi");
+    return 0;
+}
